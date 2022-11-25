5ce97d9e6b366c0a2d06ec476400c86a61066528
gus-aguiar <gustavopma90@gmail.com>
2022-11-25 15:12:16 -0300

    a dois

---
+++ src/components/SolarSystem.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <header>
+        <div data-testid="solar-system" />
+      </header>
+    );
+  }
+}
+
+export default SolarSystem;
