30b84c3588d7e43ee11b8af1aea4d979e133ed1f
gus-aguiar <gustavopma90@gmail.com>
2022-11-25 17:00:35 -0300

    fiz a seis

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
@@ -7,6 +9,10 @@ class SolarSystem extends React.Component {
       <header>
         <div data-testid="solar-system">
           <Title headline="Planetas" />
+          {planets.map(({ name, image }) => (
+
+            <PlanetCard planetName={ name } planetImage={ image } key={ name } />
+          ))}
         </div>
       </header>
     );
